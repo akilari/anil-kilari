6411405291220caf448435b58c48be625aa22da0
Anil Kilari <anilkilari@Anils-MacBook-Pro.local>
2018-09-12 21:40:30 -0400

    updated app.js

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,20 @@ import React,{ Component } from 'react';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import Banner from './banner';
 import Footer from './footer';
+import styled from 'styled-components'
 
+const Section = styled.section`
+margin: 30px 0;`
 class App extends Component {
     render() {
         return (
             <div className='container'>
-                <Banner/>
-                <Footer/>
+                <Section>
+                  <Banner/>
+                </Section>
+                <Section>
+                  <Footer/>
+                </Section>
             </div>
         );
     }
